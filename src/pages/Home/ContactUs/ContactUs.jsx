a0fa3453b2503e85cd9b3db145c4bb864be6d56c
siddiquenaim <naim.niter.tex06@gmail.com>
2023-10-15 19:22:50 +0600

    Implemented email feature

--- src/pages/Home/ContactUs/ContactUs.jsx
+++ src/pages/Home/ContactUs/ContactUs.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import { useContext, useRef } from "react";
 import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import { AuthContext } from "../../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const ContactUs = () => {
+  const { user } = useContext(AuthContext);
   const [sent, setSent] = useState(false);
+  console.log(user);
 
-  const handleMessage = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    form.reset();
-    setSent(true);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_59z174o",
+        "template_2a41fgh",
+        form.current,
+        "DQmHKD_n-mkFUg7Pu"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSent(true);
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Email has been sent successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          form.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
+
+  // const handleMessage = (event) => {
+  //   event.preventDefault();
+  //   const form = event.target;
+  //
+  //
+  // };
   return (
     <div className="w-[95%] md:w-[90%] mx-auto my-20">
       <h2
@@ -21,7 +57,7 @@ const ContactUs = () => {
         Contact Us
       </h2>
       <div className="lg:flex justify-center space-y-12">
-        <div className="flex justify-center items-center lg:w-[75%] mx-auto">
+        <div className="flex justify-center items-center  mx-auto">
           <div className="space-y-5">
             <div>
               <p className="text-[#81807D]">Email Address</p>
@@ -40,20 +76,34 @@ const ContactUs = () => {
           </div>
         </div>
         <div className="lg:w-[50%] mx-auto flex justify-center">
-          <form onSubmit={handleMessage} className="p-4 flex flex-col">
+          <form ref={form} onSubmit={sendEmail} className="p-4 flex flex-col">
+            <label htmlFor="" className="">
+              Your Name
+            </label>
+            <input
+              name="from_name"
+              type="text"
+              defaultValue={user?.displayName}
+              placeholder={user?.displayName}
+              className="input input-bordered input- input-md lg:w-[400px] mb-4 bg-slate-100"
+              required
+            />
             <label htmlFor="" className="">
               Your Email
             </label>
             <input
-              type="text"
-              placeholder="Your Email"
-              className="input input-bordered input- input-md lg:w-[400px] mb-4"
+              name="from_email"
+              type="email"
+              value={user?.email}
+              placeholder={user?.email}
+              className="input input-bordered input- input-md lg:w-[400px] mb-4 bg-slate-100"
               required
             />
             <label htmlFor="">Your Message</label>
             <textarea
-              placeholder="Your Message"
-              className="textarea textarea-bordered textarea-md lg:w-[400px]"
+              name="message"
+              placeholder="Write Your Message"
+              className="textarea textarea-bordered textarea-md lg:w-[400px] bg-slate-100"
               required
             ></textarea>
             <div className="flex justify-center items-center mt-5">
