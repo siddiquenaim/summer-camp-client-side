453ab1a1420f24e4a279200332eff04bd88555ed
siddiquenaim <naim.niter.tex06@gmail.com>
2023-10-16 22:02:49 +0600

    Updated FAQ questions

--- src/pages/Home/FAQ/FAQ.jsx
+++ src/pages/Home/FAQ/FAQ.jsx
@@ -18,52 +18,48 @@ const FAQ = () => {
           <div className="collapse collapse-plus bg-slate-100">
             <input type="radio" name="my-accordion-3" checked="checked" />
             <div className="collapse-title text-xl font-medium">
+              How many classes can I buy at once?
+            </div>
+            <div className="collapse-content">
+              <p> You can purchase an unlimited number of classes at once.</p>
+            </div>
+          </div>
+          <div className="collapse collapse-plus bg-slate-100">
+            <input type="radio" name="my-accordion-3" />
+            <div className="collapse-title text-xl font-medium">
+              How do I open an instructor option?
+            </div>
+            <div className="collapse-content">
+              <p>
+                To open an instructor option, please send us an email using the
+                "Contact Us" section below. We'll provide you with the necessary
+                information and steps to become an instructor.
+              </p>
+            </div>
+          </div>
+          <div className="collapse collapse-plus bg-slate-100">
+            <input type="radio" name="my-accordion-3" />
+            <div className="collapse-title text-xl font-medium">
+              How do I add a class as an instructor?
+            </div>
+            <div className="collapse-content">
+              <p>
+                As an instructor, you can add a class by visiting the "Add a
+                Class" section from your dashboard. Follow the provided
+                instructions to create and list your class.
+              </p>
+            </div>
+          </div>
+          <div className="collapse collapse-plus bg-slate-100">
+            <input type="radio" name="my-accordion-3" />
+            <div className="collapse-title text-xl font-medium">
               What are the payment methods accepted?
             </div>
             <div className="collapse-content">
               <p>
-                {" "}
-                We accept payments via major credit cards (Visa, MasterCard,
-                American Express) and PayPal.
-              </p>
-            </div>
-          </div>
-          <div className="collapse collapse-plus bg-slate-100">
-            <input type="radio" name="my-accordion-3" />
-            <div className="collapse-title text-xl font-medium">
-              How do I reset my password?
-            </div>
-            <div className="collapse-content">
-              <p>
-                To reset your password, go to the login page and click on the
-                "Forgot Password" link. You'll receive an email with
-                instructions on resetting your password.
-              </p>
-            </div>
-          </div>
-          <div className="collapse collapse-plus bg-slate-100">
-            <input type="radio" name="my-accordion-3" />
-            <div className="collapse-title text-xl font-medium">
-              Can I switch to a different subscription plan?
-            </div>
-            <div className="collapse-content">
-              <p>
-                Yes, you can switch to a different subscription plan at any
-                time. Visit your account settings and choose the plan that best
-                suits your needs.
-              </p>
-            </div>
-          </div>
-          <div className="collapse collapse-plus bg-slate-100">
-            <input type="radio" name="my-accordion-3" />
-            <div className="collapse-title text-xl font-medium">
-              What if I'm not satisfied with the service?
-            </div>
-            <div className="collapse-content">
-              <p>
-                We offer a 30-day money-back guarantee. If you're not satisfied
-                with our service, you can request a full refund within 30 days
-                of your purchase. Your satisfaction is our priority.
+                We accept payments through Stripe, which allows you to use major
+                credit cards (Visa, MasterCard, American Express) and other
+                payment methods for a secure and convenient transaction process.
               </p>
             </div>
           </div>
